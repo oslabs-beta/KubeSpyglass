ff671ff3e576946a00ef137ae77c38b4c0e75423
mattsaba7 <mattsabamusic@gmail.com>
2023-09-21 11:16:53 -0500

    matt updated typescript files

---
+++ client/App.tsx
@@ -0,0 +1,21 @@
+/**
+ * ************************************
+ *
+ * @module  App.jsx
+ * @author 
+ * @date
+ * @description
+ *
+ * ************************************
+ */
+
+import React from 'react';
+import NavContainer from './containers/NavContainer';
+
+const App: React.FC = () => (
+  <div id="app">
+   <NavContainer />
+  </div>
+);
+
+export default App;
