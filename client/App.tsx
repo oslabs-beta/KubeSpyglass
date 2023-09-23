b701155515a9fd88fd3c89a27f53b91cde0222fb
Andrew <adcharette@gmail.com>
2023-09-23 14:32:23 -0400

    rendered visualization within app.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -11,10 +11,12 @@
 
 import React from 'react';
 import NavContainer from './containers/NavContainer';
+import StructureVisualizationContainer from './containers/StructureVisualizationContainer';
 
 const App: React.FC = () => (
   <div id="app">
    <NavContainer />
+   <StructureVisualizationContainer />
   </div>
 );
 
