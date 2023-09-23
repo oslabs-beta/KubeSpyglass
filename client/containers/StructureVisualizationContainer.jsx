79316387da89e9845142830d9c167642c4bc7b31
Andrew <adcharette@gmail.com>
2023-09-23 14:30:59 -0400

    completed tree visualization of kubernetes cluster structure

--- client/containers/StructureVisualizationContainer.jsx
+++ client/containers/StructureVisualizationContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {useState} from 'react'
+import {useState} from 'react';
+import fs from 'fs';
 
 import Container from '@mui/material/Container'
 
@@ -8,14 +9,51 @@ import Tree from 'react-d3-tree';
 import BasicMenu from '../components/SVBasicMenu'
 
 export default function StructureVisualizationContainer() {
-  const [nodes, setNodes] = useState([]);
-  const [pods, setPods] = useState([]);
-  const [services, setServices] = useState([]);
-  const [namespaces, setNamespaces] = useState([]);
+  const [tree, setTree] = useState({});
+  
+  const buildTree = () => {
+    //currently uses the sampleStructure.json file
+    const data = fs.readFileSync('../../sampleStructure.json');
+
+    //need to implement get request to /Monitoring/structure to get the structure object
+    //const result = await axios.get('localhost:3000/monitoring/structure')
+    //const data = result.data;
+    const nodes = data.nodes;
+    const pods = data.pods;
+    const services = data.services;
+    const namespaces = data.namespaces;
+    const tree = {};
+    tree.name = 'K8s Cluster';
+    tree.children = [{name: 'nodes', children: []}, {name: 'services', children: []}, {name: 'namespaces', children: []}];
+    for(const node of nodes){
+      const nodeObj = {name: node.name, attributes: node.spec, children: [{name: 'pods', children:[]}]};
+      for(const pod of pods){
+        nodeObj.children[0].children.push({name: 'volumes', children: []});
+        nodeObj.children[0].children.push({name: 'containers', children: []});
+        for(const volume of pod.volumes){
+          nodeObj.children[0].children[0].children.push({name: volume.name, attributes: volume.projected, children: []});
+        }
+        for(const container of pod.containers){
+          nodeObj.children[0].children[1].children.push({name: container.name, attributes: container, children: []});
+        }
+      }
+      tree.children[0].children.push(nodeObj);
+    } 
+    for(const service of services){
+      tree.children[1].children.push({name: service});
+    }
+    for(const namespace of namespaces){
+      tree.children[2].children.push({name: namespace});
+    }
+    setTree(tree);
+  }
+
+  buildTree();
+
 
   return (
     <Container>
-
+      <Tree data={tree}/>
     </Container>
   )
 
