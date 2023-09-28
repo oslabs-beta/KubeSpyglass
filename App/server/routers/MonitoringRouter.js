1cf2abd24042e5eebe0755e15890e3b0e771624b
Andrew <adcharette@gmail.com>
2023-09-28 10:00:29 -0400

    moved static path to serve sample structure.json into the monitoring router

--- App/server/routers/MonitoringRouter.js
+++ App/server/routers/MonitoringRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const MonitoringRouter = express.Router();
 const MonitorController = require('../controllers/MonitorController');
 const DBController = require('../controllers/DBController');
@@ -13,6 +14,17 @@ MonitoringRouter.get('/structure/:url', MonitorController.getStructure, (req, re
 
 MonitoringRouter.get('/metrics_data', DBController.getData, (req, res) => {
   res.status(200).send(res.locals.results);
+})
+
+MonitoringRouter.get('/structure/static', (req, res) => {
+  try {
+    const data = fs.readFileSync(path.resolve(__dirname, '../../dist/assets/sampleStructure.json'), 'utf-8');
+    const jsonData = JSON.parse(data);
+    res.json(jsonData);
+  } catch (error) {
+    console.error('Error reading JSON file:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 })
 
 module.exports = MonitoringRouter;
